abddb53387c52c026b4a0d8fd0c08d4a60119e80
Rob Keimig <Rob.Keimig@pinnstrat.com>
2020-04-04 17:24:20 -0500

    better video playback TODO: Playout buffer?

--- Janus/StreamingVideoExtensions.cs
+++ Janus/StreamingVideoExtensions.cs
@@ -13,74 +13,81 @@ namespace Janus
         {
             app.Use(async (context, next) =>
             {
-                if (context.Request.Path == "/input")
+                try
                 {
-                    if (context.WebSockets.IsWebSocketRequest)
+                    if (context.Request.Path == "/input")
                     {
+                        if (context.WebSockets.IsWebSocketRequest)
+                        {
 
+                        }
+                        else
+                        {
+                            context.Response.StatusCode = 400;
+                        }
+                    }
+
+                    else if (context.Request.Path == "/video")
+                    {
+                        var width = 1280;
+                        var height = 720;
+                        var bitsPerPixel = 24;
+
+                        Bitmap drawTarget = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                        RectangleF rectf = new RectangleF(0, 0, 100, 80);
+                        Graphics g = Graphics.FromImage(drawTarget);
+                        g.SmoothingMode = SmoothingMode.None;
+                        g.InterpolationMode = InterpolationMode.NearestNeighbor;
+                        g.PixelOffsetMode = PixelOffsetMode.None;
+                        var iteration = 0;
+                        var sw = new Stopwatch();
+
+                        while (true)
+                        {
+                            var date = DateTime.UtcNow.ToString("ss ffffff");
+                            Console.WriteLine(date);
+                            g.Clear(Color.Black);
+                            var jpegsw = new Stopwatch();
+                            sw.Start();
+                            iteration++;
+                            g.FillRectangle(new SolidBrush(Color.Red), 0, 0, iteration, iteration);
+                            g.FillRectangle(new SolidBrush(Color.FromArgb(iteration % 255, iteration % 255, iteration % 255)), iteration, iteration, iteration - 5, iteration - 5);
+
+                            if (iteration > height)
+                            {
+                                iteration = 0;
+                            }
+
+                            try
+                            {
+                                sw.Stop();
+                                g.DrawString(date, new Font("Tahoma", 14), Brushes.White, rectf);
+                                g.Flush();
+                                jpegsw.Start();
+                                var jpeg = new Utility().ConvertBitmapToJpeg(drawTarget, 50);
+                                jpegsw.Stop();
+                                if (iteration % 5 == 0) { Console.WriteLine($"JPEG Encoder Latency - {jpegsw.ElapsedMilliseconds} ms ({jpegsw.ElapsedTicks} ticks) - {width}x{height} ({bitsPerPixel}bpp) = {width * height * bitsPerPixel / 1000000} Megabits - Compressed: {jpeg.Length} Bytes"); }
+                                var jpegLengthBytes = BitConverter.GetBytes(jpeg.Length);
+                                await context.Response.Body.WriteAsync(jpegLengthBytes, 0, jpegLengthBytes.Length).ConfigureAwait(false);
+                                await context.Response.Body.WriteAsync(jpeg, 0, jpeg.Length).ConfigureAwait(false);
+                                await context.Response.Body.FlushAsync().ConfigureAwait(false);
+                            }
+                            catch (Exception ex)
+                            {
+
+                            }
+
+                            Task.Delay(16).Wait();
+                        }
                     }
                     else
                     {
-                        context.Response.StatusCode = 400;
+                        await next();
                     }
                 }
-
-                else if (context.Request.Path == "/video")
-                {
-                    var width = 1280;
-                    var height = 720;
-                    var bitsPerPixel = 24;
-
-                    Bitmap drawTarget = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
-                    RectangleF rectf = new RectangleF(0, 0, 100, 80);
-                    Graphics g = Graphics.FromImage(drawTarget);
-                    g.SmoothingMode = SmoothingMode.None;
-                    g.InterpolationMode = InterpolationMode.NearestNeighbor;
-                    g.PixelOffsetMode = PixelOffsetMode.None;
-                    var iteration = 0;
-                    var sw = new Stopwatch();
-
-                    while (true)
-                    {
-                        var date = DateTime.UtcNow.ToString("ss ffffff");
-                        g.Clear(Color.Black);
-                        var jpegsw = new Stopwatch();
-                        sw.Start();
-                        iteration++;
-                        g.FillRectangle(new SolidBrush(Color.Red), 0, 0, iteration, iteration);
-                        g.FillRectangle(new SolidBrush(Color.FromArgb(iteration % 255, iteration % 255, iteration % 255)), iteration, iteration, iteration - 5, iteration - 5);
-                        
-                        if (iteration > height)
-                        {
-                            iteration = 0;
-                        }
-
-                        try
-                        {
-                            sw.Stop();
-                            g.DrawString(date, new Font("Tahoma", 14), Brushes.White, rectf);
-                            g.Flush();
-                            jpegsw.Start();
-                            var jpeg = Utility.ConvertBitmapToJpeg(drawTarget, 50);
-                            jpegsw.Stop();
-                            if (iteration % 5 == 0) { Console.WriteLine($"JPEG Encoder Latency - {jpegsw.ElapsedMilliseconds} ms ({jpegsw.ElapsedTicks} ticks) - {width}x{height} ({bitsPerPixel}bpp) = {width * height * bitsPerPixel / 1000000} Megabits - Compressed: {jpeg.Length * 8 / 1000} Kilobits"); }
-                            var jpegLengthBytes = BitConverter.GetBytes(jpeg.Length);
-                            await context.Response.Body.WriteAsync(jpegLengthBytes, 0, jpegLengthBytes.Length).ConfigureAwait(false);
-                            await context.Response.Body.WriteAsync(jpeg, 0, jpeg.Length).ConfigureAwait(false);
-                            await context.Response.Body.FlushAsync().ConfigureAwait(false);
-                        }
-                        catch(Exception ex)
-                        {
-
-                        }
-                        
-                        Task.Delay(50).Wait();
-                    }
-                }
-                else
-                {
-                    await next();
-                }
+                catch (Exception ex) 
+                { }
+                
             });
 
             return app;
--- Janus/Utility.cs
+++ Janus/Utility.cs
@@ -4,13 +4,13 @@ namespace Janus
 {
     public class Utility
     {
-        static readonly LibJpegTurbo _libJpegTurbo = new LibJpegTurbo();
+        readonly LibJpegTurbo _libJpegTurbo = new LibJpegTurbo();
 
         public static Tuple Point(float x, float y, float z) => new Tuple(x, y, z, 1.0f);
 
         public static Tuple Vector(float x, float y, float z) => new Tuple(x, y, z, 0.0f);
 
-        public static byte[] ConvertBitmapToJpeg(Bitmap bitmap, int quality)
+        public byte[] ConvertBitmapToJpeg(Bitmap bitmap, int quality)
         {
             var lockbits = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), System.Drawing.Imaging.ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
             var result = _libJpegTurbo.Compress(lockbits.Scan0, 0, bitmap.Width, bitmap.Height, LibJpegTurbo.TJPixelFormats.TJPF_RGB, LibJpegTurbo.TJSubsamplingOptions.TJSAMP_420, quality, LibJpegTurbo.TJFlags.FASTDCT);
